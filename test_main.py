38bd0fe3c6979a0f39d966b93f6d6a8cd8389ed2
Zhengming Ding <allanzmding@gmail.com>
2025-02-06 20:20:49 -0600

    Add files via upload

--- test_main.py
+++ test_main.py
@@ -2,9 +2,9 @@ from main import *
 
 def test_simple_work():
 	""" done. """
-	assert work_calc(10, 2, 2) == #TODO
-	assert work_calc(20, 3, 2) == #TODO
-	assert work_calc(30, 4, 2) == #TODO
+	assert simple_work_calc(10, 2, 2) == #TODO
+	assert simple_work_calc(20, 3, 2) == #TODO
+	assert simple_work_calc(30, 4, 2) == #TODO
 
 def test_work():
 	assert work_calc(10, 2, 2,lambda n: 1) == #TODO
@@ -15,11 +15,11 @@ def test_work():
 def test_compare_work():
 	# curry work_calc to create multiple work
 	# functions taht can be passed to compare_work
-    
+
 	# create work_fn1
 	# create work_fn2
+	res = compare_work(work_fn1, work_fn2)
 
-    res = compare_work(work_fn1, work_fn2)
 	print(res)
 
 	
